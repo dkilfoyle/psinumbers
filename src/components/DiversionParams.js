2153f6ec85bf6a31e371ffe4ae7f6d0a7737a1fd
dkilfoyle <dkilfoyle@xtra.co.nz>
2018-01-27 15:17:38 +1300

    update diversion parameters

--- src/components/DiversionParams.js
+++ src/components/DiversionParams.js
@@ -3,6 +3,6 @@ module.exports = {
   pIncidence: { name: 'pIncidence', val: 192, default: 192, type: 'number', group: 'Demographics', label: 'Stroke Incidence', helper: 'Number of strokes/100,000 adults /yr' },
   pHyperacute: { name: 'pHyperacute', val: 0.55, default: 0.55, group: 'Onset Time', label: 'Hyper-acute', helper: '% of all strokes that are hyperacute' },
   pAfterhours: { name: 'pAfterhours', val: 0.61, default: 0.61, group: 'Onset Time', label: 'After-hours', helper: '% of hyperacute stroke that present after-hours' },
-  pPASTAPos: { name: 'pPASTAPos', val: 0.65, default: 0.65, group: 'Clinical', label: 'PASTA Positive', helper: '% of hyperacute stroke that pass the PASTA screen' },
-  pMimics: { name: 'pMimics', val: 0.37, default: 0.37, group: 'Clinical', label: 'After hours', helper: '% of hyperacute stroke presenting after hours' }
+  pPASTAPos: { name: 'pPASTAPos', val: 0.50, default: 0.50, group: 'Clinical', label: 'PASTA Positive', helper: '% of hyperacute stroke that pass the PASTA screen' },
+  pMimics: { name: 'pMimics', val: 0.37, default: 0.37, group: 'Clinical', label: 'Mimics', helper: 'Additional % of PASTA that are mimics' }
 }
